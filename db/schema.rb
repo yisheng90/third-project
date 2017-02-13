0e44141ee850de7e6b7ea98e326c2df9c75a7480
sharona1610 <sharona1610@gmail.com>
2017-02-13 14:09:09 +0800

    Enquiry model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211031234) do
+ActiveRecord::Schema.define(version: 20170213054523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "enquiries", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "freelancer_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string   "description"
+    t.integer  "price"
+    t.string   "status"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["freelancer_id"], name: "index_enquiries_on_freelancer_id", using: :btree
+    t.index ["user_id"], name: "index_enquiries_on_user_id", using: :btree
+  end
+
+  create_table "freelancers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -30,4 +54,6 @@ ActiveRecord::Schema.define(version: 20170211031234) do
     t.datetime "updated_at",                  null: false
   end
 
+  add_foreign_key "enquiries", "freelancers"
+  add_foreign_key "enquiries", "users"
 end
