e1337721233a191554e75ef685ba31c1c2bf1be1
cavacado <ngzhaoloong@gmail.com>
2017-02-14 22:51:55 +0800

    ratings structure mostly done, left digo algo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213111124) do
+ActiveRecord::Schema.define(version: 20170214133954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,19 @@ ActiveRecord::Schema.define(version: 20170213111124) do
     t.index ["sender_id"], name: "index_messages_on_sender_id", using: :btree
   end
 
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "professionalism"
+    t.integer  "value"
+    t.integer  "cleanliness"
+    t.text     "description_of_job"
+    t.integer  "user_id"
+    t.integer  "freelancer_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["freelancer_id"], name: "index_ratings_on_freelancer_id", using: :btree
+    t.index ["user_id"], name: "index_ratings_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -98,4 +111,6 @@ ActiveRecord::Schema.define(version: 20170213111124) do
   add_foreign_key "freelancers", "users"
   add_foreign_key "messages", "enquiries"
   add_foreign_key "messages", "users", column: "sender_id"
+  add_foreign_key "ratings", "freelancers"
+  add_foreign_key "ratings", "users"
 end
