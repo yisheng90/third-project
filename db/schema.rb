e1934cb0c8ce4dddb40a41cf17f7c63faa9942b4
sharona1610 <sharona1610@gmail.com>
2017-02-15 09:25:47 +0800

    added location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213090550) do
+ActiveRecord::Schema.define(version: 20170214150336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20170213090550) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.text     "schedule"
+    t.string   "latitude"
+    t.string   "longitude"
     t.index ["user_id"], name: "index_freelancers_on_user_id", using: :btree
   end
 
