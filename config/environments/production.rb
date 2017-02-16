85f820cc87e6b33473c8e2cce02ed26212de703f
Yisheng90 <yisheng@yishenglee.com>
2017-02-17 01:17:01 +0800

    fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
@@ -74,7 +74,7 @@ Rails.application.configure do
 }
   config.action_mailer.perform_caching = false
 
-
+  config.action_mailer.raise_delivery_errors = false
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
